2dd2de241c1011c2ee067a906ab2625e1d5c39f6
Diego Delgado Barra <diego.delgadobarra@outlook.com>
2022-06-14 21:23:03 -0400

    segundoCommit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Funcionando");
+// Llamando axios
+const axios = requiere('axios');
+const fs = require('fs');
+
+// Condición de los parametros
+
+if(process.argv.length < 6){
+    console.log(`Este programa requiere 6 argumentos para su ejecucion`);
+    process.exit(1);
+}else{
+
+
+}
+}
